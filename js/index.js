d05160ba704ac93fc8c6893a57df6d01a861aa42
raad1masum <piraadwan@gmail.com>
2020-06-17 22:52:12 -0700

    create change image function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+function changeBackground() {
+    var backgroundsImages = ["./img/1.png", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg", "./img/5.jpg",]
+
+    document.getElementById('body').style.background = "url('./img/1.png')";
+}
+changeBackground()
+
 function displayTime() {
     var date = new Date()
     var time = date.toLocaleTimeString()
